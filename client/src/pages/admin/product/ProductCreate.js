373a881ff283287d72d8b55265b76e4d41e671a5
sulavkhadka787 <sulav.khadka787@gmail.com>
2021-02-04 20:53:46 -0700

    image upload complete

--- client/src/pages/admin/product/ProductCreate.js
+++ client/src/pages/admin/product/ProductCreate.js
@@ -5,6 +5,8 @@ import {useSelector} from "react-redux";
 import {createProduct} from "../../../functions/product";
 import ProductCreateForm from '../../../components/forms/ProductCreateForm';
 import {getCategories,getCategorySubs} from "../../../functions/category";
+import FileUpload from '../../../components/forms/FileUpload';
+import {LoadingOutlined} from '@ant-design/icons';
 
 const initialState={
         title:'Macbook Pro',
@@ -28,6 +30,7 @@ const ProductCreate=()=>{
     const [values,setValues]=useState(initialState);
     const [subOptions,setSubOptions]=useState([]);
     const [showSub,setShowSub]=useState(false);
+    const [loading,setLoading]=useState(false);
 
     const {user}=useSelector((state)=>({...state}));
 
@@ -82,10 +85,16 @@ const ProductCreate=()=>{
                 <div className="col-md-2">
                     <AdminNav />
                 </div>
+
                 <div className="col-md-10">
-                    <h4>Product Create Form</h4>
+                    {loading ? ( <LoadingOutlined className="text-danger h1" /> ) : (<h4>Product Create Form</h4>)}
+                                
                     <hr/>
-                    {JSON.stringify(values.subs)}
+                    {JSON.stringify(values.images)}
+
+                    <div className="p-3">
+                        <FileUpload values={values} setValues={setValues} setLoading={setLoading}/>
+                    </div>
 
                     <ProductCreateForm 
                         handleSubmit={handleSubmit} 
--- server/routes/cloudinary.js
+++ server/routes/cloudinary.js
@@ -10,3 +10,5 @@ const {upload,remove}=require("../controllers/cloudinary");
 
 router.post('/uploadimages',authCheck,adminCheck,upload);
 router.post('/removeimage',authCheck,adminCheck,remove);
+
+module.exports=router;
