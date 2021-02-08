967a678c633b2dc6aac862f36f0141e1491afd83
sulavkhadka787 <sulav.khadka787@gmail.com>
2021-02-07 20:30:26 -0700

    admin-product-delete

--- client/src/functions/product.js
+++ client/src/functions/product.js
@@ -6,3 +6,14 @@ export const createProduct=async(product,authtoken)=>
             authtoken
         }
     })
+
+export const getProductByCount=async(count)=>
+    await axios.get(`${process.env.REACT_APP_API}/products/${count}`);
+
+
+export const removeProduct = async (slug, authtoken) =>
+    await axios.delete(`${process.env.REACT_APP_API}/product/${slug}`, {
+      headers: {
+        authtoken,
+      },
+    });
