97e0426284a435f1cbfe52d0674a2b9d82085b77
sulavkhadka787 <sulav.khadka787@gmail.com>
2021-02-14 11:59:58 -0700

    adding new arrivals and bestsellers

--- server/routes/product.js
+++ server/routes/product.js
@@ -5,7 +5,7 @@ const router=express.Router();
 const {authCheck,adminCheck}=require("../middlewares/auth");
 
 //controller
-const {create,listAll,remove,read,update}=require("../controllers/product");
+const {create,listAll,remove,read,update,list}=require("../controllers/product");
 
 //routes
 router.post("/product",authCheck,adminCheck,create);
@@ -13,5 +13,7 @@ router.get('/products/:count',listAll);
 router.delete("/product/:slug", authCheck, adminCheck, remove);
 router.get('/product/:slug',read);
 router.put("/product/:slug",authCheck,adminCheck,update);
+
+router.post("/products",list);
 
 module.exports=router;
