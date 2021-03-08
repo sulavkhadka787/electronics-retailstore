2d260b74b80f60d5c181149a596819054506a3f9
sulavkhadka787 <sulav.khadka787@gmail.com>
2021-03-07 19:38:18 -0700

    update quantity in cart

--- client/src/components/cards/ProductCardInCheckout.js
+++ client/src/components/cards/ProductCardInCheckout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ModalImage from 'react-modal-image';
 import laptop from '../../images/laptop.png';
 import {useDispatch} from 'react-redux';
+import {toast} from 'react-toastify';
 
 const ProductCardInCheckout=({p})=>{
 
@@ -24,6 +25,34 @@ const ProductCardInCheckout=({p})=>{
             })
 
             console.log('cart update color',cart);
+            localStorage.setItem("cart",JSON.stringify(cart));
+            dispatch({
+                type:'ADD_TO_CART',
+                payload:cart
+            })
+        }
+    }
+
+    const handleQuantityChange=(e)=>{
+        console.log('quanti',p.quantity);
+        let count=e.target.value < 1 ? 1:e.target.value
+        if(count>p.quantity){
+            toast.error(`Max available quantity: ${p.quantity}`);
+            return;
+        }
+        let cart=[];
+        if(typeof window !=="undefined"){
+            if(localStorage.getItem("cart")){
+                cart=JSON.parse(localStorage.getItem("cart"))
+            }
+
+            cart.map((product,i)=>{
+                if(product._id == p._id){
+                    cart[i].count=count;
+                }
+                
+            });
+
             localStorage.setItem("cart",JSON.stringify(cart));
             dispatch({
                 type:'ADD_TO_CART',
@@ -64,6 +93,9 @@ const ProductCardInCheckout=({p})=>{
                             </option>
                         ))}
                     </select>
+                </td>
+                <td className="text-center">
+                  <input type='number' className="form-control" value={p.count} onChange={handleQuantityChange} />
                 </td>
                 <td>  Shipping icon  </td>
                 <td>  Delete icon  </td>
