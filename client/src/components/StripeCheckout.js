f38abda2d70467f145bf184afe711fed62a24412
sulavkhadka787 <sulav.khadka787@gmail.com>
2021-03-19 18:40:57 -0600

    sucessful payment using dummy credit card

--- client/src/components/StripeCheckout.js
+++ client/src/components/StripeCheckout.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import { useSelector, useDispatch } from "react-redux";
 import { createPaymentIntent } from "../functions/stripe";
+import {Link} from 'react-router-dom';
 
 const StripeCheckout = ({ history }) => {
   const dispatch = useDispatch();
@@ -24,11 +25,37 @@ const StripeCheckout = ({ history }) => {
   }, []);
 
   const handleSubmit = async (e) => {
-    //
+    e.preventDefault();
+    setProcessing(true);
+    //console.log('eee',e.target.name.value);
+    const payload=await stripe.confirmCardPayment(clientSecret,{
+      payment_method:{
+        card:elements.getElement(CardElement),
+        billing_details:{
+          name:e.target.name.value
+        }
+      }
+    });
+
+    if(payload.error){
+      setError(`Payment failed ${payload.error.message}`)
+      setProcessing(false);
+    }else{
+      //
+      console.log(JSON.stringify(payload,null,4));
+      setError(null);
+      setProcessing(false);
+      setSucceeded(true);
+    }
   };
 
   const handleChange = async (e) => {
-    //
+    //listen for changes in card element
+    //and display any error as the customer types their card details
+    console.log('eeeee',e.empty);
+    setDisabled(e.empty); //disable pay button if errors
+    setError(e.error ? e.error.message:"")
+
   };
 
   const cartStyle = {
@@ -51,6 +78,10 @@ const StripeCheckout = ({ history }) => {
 
   return (
     <>
+    <p className={succeeded ? 'result-message':'result-message hidden'}>
+      Payment Successful.{" "}
+      <Link to="/user/history">See it in your purchase history</Link>
+    </p>
       <form id="payment-form" className="stripe-form" onSubmit={handleSubmit}>
         <CardElement
           id="card-element"
@@ -65,6 +96,12 @@ const StripeCheckout = ({ history }) => {
             {processing ? <div className="spinner" id="spinner"></div> : "Pay"}
           </span>
         </button>
+        <br/>
+        {error && (
+          <div className="card-error" role="alert">
+            {error}
+          </div>
+        )}
       </form>
     </>
   );
--- server/server.js
+++ server/server.js
@@ -34,3 +34,5 @@ readdirSync('./routes').map((r)=>app.use("/api",require("./routes/"+r)));
 const port=process.env.PORT||8000;
 
 app.listen(port,()=>console.log(`Server is running on port ${port}`));
+
+
