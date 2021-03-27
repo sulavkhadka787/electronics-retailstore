f70a4de4cffcad703563a03af44e8e8ab4e9851f
sulavkhadka787 <sulav.khadka787@gmail.com>
2021-03-26 20:57:30 -0600

    show each order's product in table

--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -19,6 +19,44 @@ const History=()=>{
             setOrders(res.data);
         })
 
+    const showOrderInTable=(order)=>
+        <table className="table table-bordered">
+            <thead className="thead-light">
+                <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Brand</th>
+                    <th scope="col">Color</th>
+                    <th scope="col">Count</th>
+                    <th scope="col">Shipping</th>                    
+                </tr>
+            </thead>
+            <tbody>
+                {order.products.map((p,i)=>(
+                    <tr key={i}>
+                        <td><b>{p.product.title}</b></td>
+                        <td>{p.product.price}</td>
+                        <td>{p.product.brand}</td>
+                        <td>{p.color}</td>
+                        <td>{p.count}</td>
+                        <td>{p.product.shipping==="Yes" ? 
+                            <CheckCircleOutlined style={{color:"green"}} /> : <CloseCircleOutlined style={{color:"red"}}/>}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    const showEachOrders=()=>orders.map((order,i)=>(
+        <div key={i} className="m-5 p-3 card">
+                <p>Show Payment Info</p>
+                {showOrderInTable(order)}
+                <div className="row">
+                    <div className="col">
+                        <p>PDF download</p>
+                    </div>
+                </div>
+        </div>
+    ))
+
     return(
         <div className="container-fluid">
         
@@ -26,7 +64,12 @@ const History=()=>{
                 <div className="col-md-2">
                     <UserNav />
                 </div>
-                <div className="col">user history page</div>
+                <div className="col text-center">
+                    <h4>
+                        {orders.length > 0 ? "User purchase orders" : "No purchase orders"}
+                    </h4>
+                    {showEachOrders()}
+                </div>
             </div>
         </div>
     )
